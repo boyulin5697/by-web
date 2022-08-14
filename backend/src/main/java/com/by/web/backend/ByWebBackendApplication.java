c49c604761d5f6dec10c6ac7c043a57b0365084f
boyulin <1404916254@qq.com>
2022-08-14 23:13:58 +0800

    后端初始化

---
+++ backend/src/main/java/com/by/web/backend/ByWebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.by.web.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByWebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByWebBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/by/web/backend/ByWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.by.web.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByWebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
